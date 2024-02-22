ba53d88da383b94cc6b76c4d7ca49b3f1a5460f7
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2024-02-23 04:17:43 +0530

    Make Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,13 @@
 const Footer = () => {
     return (
-        <footer>
-            <p>© 2024</p>
-        </footer>
+        <div className="h-24 flex items-center justify-between">
+            <div className="font-bold">THEFZNKHAN</div>
+            <div className="text-sm">
+            THEFZNKHAN creative thoughts agency © All rights reserved.
+            </div>
+        </div>
     );
 };
 
 export default Footer;
+ 
